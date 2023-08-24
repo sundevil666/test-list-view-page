90793bdfe8622ea6a404ab05bbf1a12d02653bc4
sundevildi <sundevildi@gmail.com>
2023-08-24 19:05:38 +0200

    commit

--- js/chunk-2d0aed93.aac0f6d7.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0aed93"],{"0c8b":function(n,c,e){"use strict";e.r(c);var t=e("7a23");const o={class:"col"},a=Object(t["g"])("h1",null,"Not Found Page ",-1),u=Object(t["g"])("div",null,"(Page Marketing only works)",-1),d=[a,u];function r(n,c){return Object(t["u"])(),Object(t["f"])("div",o,d)}var s=e("6b0d"),b=e.n(s);const i={},l=b()(i,[["render",r]]);c["default"]=l}}]);
-//# sourceMappingURL=chunk-2d0aed93.aac0f6d7.js.map
